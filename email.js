4857b21595bb1e87b0cc720267533193b3f22cac
Dave Poole <davethegr8@gmail.com>
2013-03-07 10:10:12 -0800

    Update email.js

--- email.js
+++ email.js
@@ -1,3 +1,7 @@
+// Had a login form coming from an api that included fields for email input
+// with type="text". This snippet gave all the email inputs the proper type
+// to change to the email keyboard layout on mobile devices.
+
 $('.email').each(function () {
     var html = $(this).wrap('<div>').parent().html().replace(/type=\"text\"/, 'type="email"');
     $(html).insertAfter(this).prev().remove();
