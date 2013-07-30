ca9e3c5fc709f945a764198731e60232f1ae9bc7
Dave Poole <davethegr8@gmail.com>
2013-07-30 11:03:28 -0700

    added "die var dump" function

--- debug.php
+++ debug.php
@@ -1,1 +1,15 @@
 echo __FILE__, ':', __FUNCTION__, '('.implode(', ', func_get_args()).') @ line ', __LINE__, "\n";
+
+//Die var dump
+function dvd() {
+    $backtrace = debug_backtrace();
+    $last = array_pop($backtrace);
+
+    echo 'die called from '.$last['file'].' line '.$last['line'], "\n";
+
+    foreach(func_get_args() as $arg) {
+        echo var_export($arg, true), "\n";
+    }
+
+    die();
+}
