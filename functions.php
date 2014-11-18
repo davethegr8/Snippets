03932ce6c2765b3c5765f2dedc7ffe543f51be30
Dave Poole <davethegr8@users.noreply.github.com>
2014-11-18 11:20:16 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,41 @@
 <?php
 
+/**
+ * A way of doing factorial without writing math
+ */
 function factorial($n) {
     return array_product(range(1, max($n, 1)));
 }
 
+/** 
+ * Rounds to the nearest multiple of $interval. eg 4,8,12 etc
+ */
 function roundInterval($num, $interval) {
     return round($num / $interval) * $interval;
+}
+
+/**
+ * Checks if all items in $checks are trueish. AKA AND.
+ */
+function all(array $checks) {
+    foreach($checks as $item) {
+        if(!$item) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+/**
+ * Checks if any item in $checks is trueish. AKA OR
+ */
+function any(array $checks) {
+    foreach($checks as $item) {
+        if($item) {
+            return true;
+        }
+    }
+    
+    return false;
 }
