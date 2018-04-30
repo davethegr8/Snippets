caa9f5689272c01901dc252f4c910ad76c714819
Dave <davethegr8@users.noreply.github.com>
2018-04-30 10:42:04 -0700

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,2 +1,5 @@
 # Finds files bigger than 10M and displays size
 find . -not -iwholename '*.git*' -type f -size +10M | xargs -I {} du -sh {} 
+
+# run something with ruby
+docker run --rm -v `pwd`:/usr/src/ -w /usr/src ruby:2.5
